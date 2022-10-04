bd40338fdf4f96b0845afd3d6ee7c433b35e885b
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-10-04 11:19:53 +0600

    Model component

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 import { ReactComponent as Close } from '../assets/icons/close.svg';
@@ -5,10 +6,16 @@ import { ReactComponent as Close } from '../assets/icons/close.svg';
 export default function Model({ open, children, close }) {
   if (!open) return null;
 
+  window.addEventListener('click', (e) => {
+    if (e.target.classList.contains('modal')) {
+      close();
+    }
+  });
+
   return (
     <div>
 
-      <div className="model-overlay">
+      <div className="model-overlay modal">
         <div className="model-container">
 
           <button className="model-close" type="button" onClick={close}>
