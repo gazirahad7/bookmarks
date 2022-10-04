8d19eb52ef0137c32cafc9f0f86c6d2d900338a8
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-10-04 11:23:30 +0600

    Add: search bar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 
 import { ReactComponent as Search } from '../assets/icons/search.svg';
 
+import AddList from './AddList';
+
 export default function Searchbar() {
   return (
 
-    <div className="searchbar">
-      <input type="text" placeholder="Search" />
-      <div className="search-icon">
-        <Search />
+    <div className="search-cont">
+      <div className="searchbar">
+        <input type="text" placeholder="Search" />
+        <div className="search-icon">
+          <Search />
+        </div>
       </div>
+
+      <div className="add-icon">
+
+        <AddList />
+      </div>
+
     </div>
 
   );
