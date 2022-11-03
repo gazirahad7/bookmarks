4197e0842937f33686203c4a1c971163e27ff7aa
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-11-03 15:56:59 +0600

    Add: toast notification

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,18 +1,18 @@
-/* eslint-disable consistent-return */
+/* eslint-disable react/prop-types */
 /* eslint-disable max-len */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import Tags from '@yaireo/tagify/dist/react.tagify';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { ReactComponent as AddIcon } from '../assets/icons/add-item.svg';
+import { ReactComponent as Edit } from '../assets/icons/edit.svg';
 import { categorySuggestions, tagsSuggestions } from '../data';
 import { useBookmarksContext } from '../hooks/useBookmarksContext';
 import BookmarksServices from '../services/bookmarks.services';
 import Model from './Model';
 import { Label } from './styles/Elements.styles';
 
-export default function AddList() {
+export default function Update({ id }) {
   const { dispatch } = useBookmarksContext();
   const [show, setShow] = React.useState(false);
   const [title, setTitle] = React.useState('');
@@ -20,12 +20,27 @@ export default function AddList() {
   const [category, setCategory] = React.useState(null);
   const [tags, setTags] = React.useState([]);
 
+  const getBookmark = async (listID) => {
+    const data = await BookmarksServices.singleList(listID);
+    /*  console.log('xxxxx', data.data().title);
+    console.log('yyyy', data.data().tags);
+    setTitle(data._document.data.value.mapValue.fields.title.stringValue);
+    setUrl(data._document.data.value.mapValue.fields.url.stringValue);
+    setCategory(data._document.data.value.mapValue.fields.category.arrayValue.values.map((item) => item.stringValue));
+    setTags(data._document.data.value.mapValue.fields.tags.arrayValue.values.map((item) => item.stringValue)); */
+
+    setTitle(data.data().title);
+    setUrl(data.data().url);
+    setCategory(data.data().category);
+    setTags(data.data().tags);
+  };
+
   // Tagify settings object
   const baseTagifySettings = (maxTags) => ({
     blacklist: [],
     maxTags: maxTags || 5,
     backspace: 'edit',
-    placeholder: 'type something',
+    placeholder: '',
     editTags: 1,
     dropdown: {
       enabled: 0,
@@ -57,47 +72,39 @@ export default function AddList() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    if (!title || !url || !category || !tags) {
+      toast.error('Please fill all the fields');
+      return;
+    }
+    const list = {
+      title,
+      url,
+      category,
+      tags,
+    };
     try {
-      if (!title || !url || !category || !tags) {
-        toast.error('Please fill all the fields');
-        return false;
-      }
-      const list = {
-        title,
-        url,
-        category,
-        tags,
-      };
-      const response = await BookmarksServices.addList(list);
+      await BookmarksServices.updateList(id, list);
       const data = await BookmarksServices.getBookmarksList();
-      console.log('responseid ', response.id);
-      if (response.id) {
-        dispatch({ type: 'SET_BOOKMARKS', payload: data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) });
-        toast.success('Added successfully');
-        setShow(false);
-        setTitle('');
-        setUrl('');
-        setCategory(null);
-        setTags([]);
-      }
-      // check response success or not
+      dispatch({ type: 'SET_BOOKMARKS', payload: data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) });
+      toast.success('Updated successfully');
+      setShow(false);
     } catch (err) {
       console.log(err);
     }
   };
-
-  console.log('Render... AddList');
+  useEffect(() => {
+    getBookmark(id);
+  }, [id]);
+  console.log('Render... Update');
   return (
     <div>
       <button type="button" onClick={() => setShow(true)}>
-        <AddIcon />
+        <Edit className="action-icon" />
       </button>
-      <Model open={show} close={() => setShow()}>
-        {/* <ToastContainer /> */}
+      <Model open={show} close={() => setShow(false)}>
         <div className="add-list-cont">
           <form onSubmit={handleSubmit}>
-            <h3>Add new bookmark</h3>
+            <h3>Update Bookmarks</h3>
             <div>
               <Label>Title</Label>
               <input type="text" placeholder="Add title" onChange={(e) => setTitle(e.target.value)} value={title} />
@@ -108,14 +115,14 @@ export default function AddList() {
             </div>
             <div>
               <Label>Category</Label>
-              <Tags name="tags" settings={settings(categorySuggestions, 1, handleChangeCategory)} />
+              <Tags name="tags" settings={settings(categorySuggestions, 1, handleChangeCategory)} initialValue={[category]} />
             </div>
             <div>
               <Label>Tags</Label>
-              <Tags name="tags" settings={settings(tagsSuggestions, 5, handleChangeTags)} />
+              <Tags name="tags" settings={settings(tagsSuggestions, 5, handleChangeTags)} initialValue={[tags]} />
             </div>
-            <div />
-            <button className="btn" type="submit">Add</button>
+
+            <button className="btn" type="submit">Update</button>
           </form>
         </div>
       </Model>
