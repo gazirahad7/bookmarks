9e72aa21d5bd9a61cd437bc6d31902c1770abb0e
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-11-03 15:58:51 +0600

    Feature: searching bookmarks

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-
+import { ReactComponent as AddIcon } from '../assets/icons/add-item.svg';
 import { ReactComponent as Search } from '../assets/icons/search.svg';
-
 import AddList from './AddList';
 
-export default function Searchbar() {
+// eslint-disable-next-line react/prop-types
+export default function Searchbar({ getSearchData }) {
+  const [data, setData] = React.useState([]);
+  const handleSearch = (e) => {
+    setData(e.target.value);
+    getSearchData(data);
+  };
   return (
-
     <div className="search-cont">
       <div className="searchbar">
-        <input type="text" placeholder="Search" />
+        <input type="text" placeholder="Search" onChange={handleSearch} />
         <div className="search-icon">
           <Search />
         </div>
       </div>
-
       <div className="add-icon">
-
-        <AddList />
+        <AddList icon={<AddIcon />} />
       </div>
-
     </div>
-
   );
 }
